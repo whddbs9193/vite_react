20b38c323dbed8725c16709142120b460bb8eea2
crown <crown@srpost.co.kr>
2025-10-15 17:45:26 +0900

    인텔리제이 사용하지 않는 상수 오류문구 경고문구로 변경

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,21 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+      rules: {
+          'no-unused-vars': 'off',
+          '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
+      }
   },
 ])
+
+/* 인텔리 제이 에서 react 사용하지않는 상수 오류 에서 경고로 바꾸는 방법
+*   1. File > Settings > Languages & Frameworks > JavaScript > Code Quality Tools > ESLint , automatic(자동) 으로 변경
+*
+*   2. eslint.config.js -> rules 추가
+*      rules: {
+          'no-unused-vars': 'off',
+          '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
+      }
+      *
+*   3. tsconfig.app.json -> noUnusedLocals : false 로 변경 하면 오류에서 경고로 변경됨.
+* */
